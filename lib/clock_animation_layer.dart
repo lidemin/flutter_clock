78bb55ef8ae37c7aaedc17a755e91d0007be2312
damon <damon@insight.co>
2020-01-01 17:43:19 +1100

    Added a simple animation on that layer, and it works pretty okay on iPhone but not on browser. it takes 100% CPU time on chrome.

--- lib/clock_animation_layer.dart
+++ lib/clock_animation_layer.dart
@@ -5,11 +5,39 @@ class ClockAnimationLayer extends StatefulWidget {
   _ClockAnimationLayerState createState() => _ClockAnimationLayerState();
 }
 
-class _ClockAnimationLayerState extends State<ClockAnimationLayer> {
+class _ClockAnimationLayerState extends State<ClockAnimationLayer>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+      reverseDuration: const Duration(seconds: 5),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black.withOpacity(0.9),
+    return AnimatedBuilder(
+      animation: _controller,
+      child: Container(
+        color: Colors.black,
+      ),
+      builder: (BuildContext context, Widget child) {
+        return Opacity(
+          opacity: _controller.value,
+          child: child,
+        );
+      },
     );
   }
 }
